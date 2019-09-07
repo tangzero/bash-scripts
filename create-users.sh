4c2ee6bef0c96874733d2d8b333d14a0e45d39bc
Jairo Luiz <t4ngz3r0@gmail.com>
2019-09-06 22:02:05 -0300

    bash script to create users based on a txt file

---
+++ create-users.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function main() {
+	clear
+
+	filename=$1
+	if [[ ! -e $filename ]]; then
+		echo "Input file $filename do not exist!"
+		exit 1
+	fi
+
+	cat $filename | while read user; do
+		if [ ! -z "$user" ]; then
+			create_user $user
+		fi
+	done
+}
+
+function create_user() {
+	user=$1
+	pass=$2
+	useradd $user
+	echo $pass | passwd $user --stdin
+	echo "User $user successful created!"
+}
+
+# start the script
+main $1
